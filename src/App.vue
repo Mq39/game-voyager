1de57c9f525688c44c2c06c72936748f17da57f3
Mq39 <predragmitr@gmail.com>
2026-03-04 18:40:53 +0100

    Fixed animation restarting on new load
    Moved animation to App.vue so it doesn't remount every time the user goes to a new location

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,56 @@
 </script>
 
 <template>
+  <div class="stars"></div>
+  <div class="particles"></div>
   <div class="container">
     <RouterView />
   </div>
 </template>
 
-<style scoped></style>
+<style>
+body {
+  background: linear-gradient(270deg,
+      #0b0f1a,
+      #1a2238,
+      #12172a,
+      #0b0f1a);
+
+  background-size: 400% 400%;
+
+}
+
+@keyframes spaceGradient {
+  0% {
+    background-position: 0% 50%;
+  }
+
+  50% {
+    background-position: 100% 50%;
+  }
+
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+.stars {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: url("https://www.transparenttextures.com/patterns/stardust.png");
+  animation: starsMove 120s linear infinite;
+  opacity: 0.25;
+  z-index: -1;
+}
+
+@keyframes starsMove {
+  from {
+    background-position: 0 0;
+  }
+
+  to {
+    background-position: 2000px 1000px;
+  }
+}
+</style>
--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -3,8 +3,7 @@ const year = new Date().getFullYear()
 </script>
 
 <template>
-    <div class="stars"></div>
-    <div class="particles"></div>
+
     <div class="d-flex align-items-center">
         <RouterLink class="nav-link" to="/" active-class="active">
             <img src="../assets/GameVoyagerLogo.png" style="height:120px;" alt="Logo" class="me-3">
@@ -100,50 +99,5 @@ const year = new Date().getFullYear()
 
 .nav-link:hover {
     color: #7C5CFF;
-}
-
-body {
-    background: linear-gradient(270deg,
-            #0b0f1a,
-            #1a2238,
-            #12172a,
-            #0b0f1a);
-
-    background-size: 400% 400%;
-
-}
-
-@keyframes spaceGradient {
-    0% {
-        background-position: 0% 50%;
-    }
-
-    50% {
-        background-position: 100% 50%;
-    }
-
-    100% {
-        background-position: 0% 50%;
-    }
-}
-
-.stars {
-    position: fixed;
-    width: 100%;
-    height: 100%;
-    background: url("https://www.transparenttextures.com/patterns/stardust.png");
-    animation: starsMove 120s linear infinite;
-    opacity: 0.25;
-    z-index: -1;
-}
-
-@keyframes starsMove {
-    from {
-        background-position: 0 0;
-    }
-
-    to {
-        background-position: 2000px 1000px;
-    }
 }
 </style>
