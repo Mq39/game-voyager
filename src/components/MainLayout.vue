b0b446f4f3861f696a3f1c6f15ff214e0d784fa3
Mq39 <predragmitr@gmail.com>
2026-03-04 13:54:36 +0100

    Main layout, Discover, Browse, Logo
    Added Main layout, Discover component, Browse component, imported logo

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+const year = new Date().getFullYear()
+</script>
+
+<template>
+    <div class="d-flex align-items-center">
+        <RouterLink class="nav-link" to="/" active-class="active">
+            <img src="../assets/GameVoyagerLogo.png" style="height:120px;" alt="Logo" class="me-3">
+        </RouterLink>
+
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary flex-grow-1">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav p-0 me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" to="/discover" active-class="active">
+                                Discover
+                            </RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink class="nav-link" to="/browse" active-class="active">
+                                Browse
+                            </RouterLink>
+                        </li>
+                    </ul>
+                    <div class="navbar-text">
+                        Ime Korisnika
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+    </div>
+    <slot></slot>
+    <footer class="mt-3 text-center">&copy; {{ year }} - GameVoyager</footer>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,46 @@
+import App from '@/App.vue'
+import Browse from '@/Browse.vue'
+import Discover from '@/Discover.vue'
+import Home from '@/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: Home,
+
+      meta: {
+        title: 'Home'
+      }
+    },
+
+    {
+      path: '/discover',
+      component: Discover,
+
+      meta: {
+        title: 'Discover'
+      }
+    },
+
+    {
+      path: '/browse',
+      component: Browse,
+
+      meta: {
+        title: 'Browse'
+      }
+    }
+
+  ],
+})
+
+router.afterEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = `${to.meta.title}`
+  }
 })
 
 export default router
